caa7e83d4f14ffc702edb4aee13f36bbb587c9e4
Muh. Wardiman <wardimanxixv@hotmail.com>
2020-09-25 18:12:15 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -15,8 +15,7 @@ var urlsToCache = [
   'https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.18.0/trumbowyg.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
   'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.18.0/ui/trumbowyg.min.css',
-  '/sw.js'
+  'https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.18.0/ui/trumbowyg.min.css'
 ];
 
 self.addEventListener('install', function (event) {
